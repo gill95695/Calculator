7e58dc382f69e4ac9826e5d4e275b8be95af1ce3
Amanjot Gill <amanjotgill95695@gmail.com>
2022-07-13 19:26:06 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Aman Gill on 2022-07-13.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
